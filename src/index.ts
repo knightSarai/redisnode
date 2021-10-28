ebdd00a89a9a7dc724e17e39384b0fe78c88812a
knight <knight.sarai.dev@gmail.com>
2021-10-29 00:06:24 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import redis from 'redis';
+
+const app = express();
+const client = redis.createClient();
+
+client.set('visits', '0');
+
+app.get('/', (req: Request, res: Response) => {
+  client.get('visits', (err, visits) => {
+    console.log(err);
+    if (visits) {
+      res.send('Number of visits is: ' + visits);
+      client.set('visits', `${parseInt(visits) + 1}`);
+    }
+  });
+  res.send('');
+});
+
+app.listen('8080', () => console.log('App on port 8080'));
